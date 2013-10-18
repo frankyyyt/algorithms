4202069a11a4761d670648ebbfc66c2c60a49ac7
Andrew Ehrlich <ehrlichja@gmail.com>
2013-10-18 15:50:07 -0700

    a very slow method

--- euler/003.py
+++ euler/003.py
@@ -6,5 +6,28 @@ What is the largest prime factor of the number 600851475143 ?
 
 """
 
-n = 600851475143
+def find_prime_factors(n):
+	result = list()
 
+	factor = n
+
+	while factor > 1:
+		print "checking factor", factor
+		if n % factor == 0:
+			print factor, "is a factor"
+			if is_prime(factor) == True:
+				result.append(factor)
+		factor -= 1
+	return result
+
+def is_prime(n):
+	if n % n == 0:
+		x = n - 1
+		while x > 1:
+			print "testing if prime:", n, "%", x
+			if n % x == 0:
+				return False
+			x -= 1
+	return True
+
+print find_prime_factors(600851475143)
