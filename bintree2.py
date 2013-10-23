968a32e110eccbe65ac6e9bc123f4757bb8b9acc
Andrew Ehrlich <ehrlichja@gmail.com>
2013-10-23 16:54:37 -0700

    more traversal

--- bintree2.py
+++ bintree2.py
@@ -1,9 +1,10 @@
 class Node():
 
 	def __init__(self, value):
-		self.left_child = None
-		self.right_child = None
-		self.value = value
+		self.left_child 	= None
+		self.right_child 	= None
+		self.parent 		= None
+		self.value 			= value
 
 	def get_left_child(self):
 		return self.left_child
@@ -11,30 +12,43 @@ class Node():
 	def get_right_child(self):
 		return self.right_child
 
+	def get_parent(self):
+		return self.parent
+
 	def get_value(self):
 		return self.value
 
 	def insert_left_child(self, new_node):
 		if self.left_child == None:
 			self.left_child = Node(new_node)
+			self.left_child.parent = self
 
 	def insert_right_child(self, new_node):
 		if self.right_child == None:
 			self.right_child = Node(new_node)
+			self.right_child.parent = self
 
 def traverse(node):
 	print node.value
+
+	if node.parent != None:
+		print "( parent is", node.parent.value, ")"
+
+	if node.left_child == None and node.right_child == None:
+		print "I reached the bottom!"
+
 	if node.left_child != None:
 		traverse(node.left_child)
-	#else: 
-		# go up one level and check right child
 
+	if node.right_child != None:
+		traverse(node.right_child)
+	
+
+		
 root = Node("root")
 root.insert_left_child("alibi")
 root.left_child.insert_left_child("thrud")
 root.left_child.insert_right_child("muck")
 root.left_child.left_child.insert_left_child("kreep")
-#print root.left_child.left_child.left_child
 
-node = root
-traverse(node)
+traverse(root)
