3229ac0b724360855b53da08d73587a5f556da22
Andrew Ehrlich <ehrlichja@gmail.com>
2013-10-23 17:19:14 -0700

    no message

--- bintree2.py
+++ bintree2.py
@@ -17,22 +17,29 @@ class Node():
 			self.right_child.parent = self
 
 def traverse(node):
+	global node_count, max_depth, depth
+
+	node_count += 1
+	depth += 1
+
 	print node.value
 
 	if node.parent != None:
-		print "( parent is", node.parent.value, ")"
+		print "	( parent is", node.parent.value, ")"
 
 	if node.left_child == None and node.right_child == None:
-		print "I reached the bottom!"
+		print "	I reached the bottom at depth", depth
+		if depth > max_depth:
+			max_depth = depth
 
 	if node.left_child != None:
 		traverse(node.left_child)
+		depth -= 1
 
 	if node.right_child != None:
 		traverse(node.right_child)
+		depth -= 1
 	
-
-		
 root = Node("root")
 root.insert_left_child("alibi")
 root.insert_right_child("klipper")
@@ -41,5 +48,14 @@ root.right_child.insert_right_child("burp")
 root.left_child.insert_left_child("thrud")
 root.left_child.insert_right_child("muck")
 root.left_child.left_child.insert_left_child("kreep")
+root.left_child.left_child.left_child.insert_left_child("flinger")
+
+node_count = 0
+depth = 0
+max_depth = 0
 
 traverse(root)
+
+print " "
+print "Maximum depth of the tree is", max_depth
+print "Number of nodes is:", node_count
